306cfa99b16d8694d0a0a15809e388f3f3e273c6
liuSir <865898963@qq.com>
2021-08-17 17:59:30 +0800

    新增测试

--- src/Facades/OpenPlatForm.php
+++ src/Facades/OpenPlatForm.php
@@ -8,6 +8,14 @@ namespace LiuDev\WxOpenPlatForm\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * User: liuSir
+ * Date: 2021/8/17
+ * Class OpenPlatForm
+ * @package LiuDev\WxOpenPlatForm\Facades
+ * @method static \LiuDev\WxOpenPlatForm\OpenPlatForm testRun()
+ * @method static \LiuDev\WxOpenPlatForm\OpenPlatForm testH()
+ */
 class OpenPlatForm extends Facade
 {
     protected static function getFacadeAccessor()
